0e53fe865f4d415e142de5465b09ea0e543ae32e
jerome11sgic <jerome11sgic@gmail.com>
2020-03-19 08:45:15 +0530

    changes done

---
+++ src/main/java/com/batch4/demoinstructorapp/DemoInstructorAppApplication.java
@@ -0,0 +1,13 @@
+package com.batch4.demoinstructorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInstructorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInstructorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch4/demoinstructorapp/DemoInstructorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch4.demoinstructorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInstructorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
